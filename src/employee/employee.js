6e233c1f3a31bd75b9b322ad35585a61eb8313e7
ardiantrik-mts <ArdianTri.Kusuma@mitrais.com>
2021-06-16 14:58:26 +0700

    do the jsbasic task

--- src/employee/employee.js
+++ src/employee/employee.js
@@ -13,8 +13,28 @@ module.exports.Employee = class Employee {
     return { name: this.name, email: this.email, password: this.password };
   }
 
+  getUsers() {
+    return fetch(`https://jsonplaceholder.typicode.com/users`)
+      .then(result => {
+        return result.json();
+      })
+      .catch(err => {
+        throw(err);
+      });
+  }
+
   getUserPosts(userId) {
     return fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
+      .then(result => {
+        return result.json();
+      })
+      .catch(err => {
+        throw(err);
+      });
+  }
+
+  getPostComments(postId) {
+    return fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
       .then(result => {
         return result.json();
       })
--- src/index.js
+++ src/index.js
@@ -18,11 +18,62 @@ module.exports = {
     // status.innerHTML = 'End';
   },
   displayPosts: () => {
-    const employee = new Employee();
-    employee.getUserPosts(1)
-      .then(result => {
-        console.log(result)
+    function fetchUsers(item) {
+      var para = document.createElement("li");
+      para.addEventListener("click", function() {
+        document.getElementById("postGrid").innerHTML = '';
+        document.getElementById("commentGrid").innerHTML = '';
+        employee.getUserPosts(item['id'])
+        .then(result => {
+          result.forEach(fetchPosts);
+        });
+        // console.log("cek");
       });
+        para.innerHTML = `<h6>`+item['name']+`</h6>`;
+        para.classList.add("list-group-item");
+        document.getElementById("userGrid").appendChild(para);
+    }
+
+    function fetchPosts(item) {
+      var cardDiv = document.createElement("li");
+      cardDiv.classList.add("list-group-item");
+      cardDiv.addEventListener("click", function() {
+        document.getElementById("commentGrid").innerHTML = '';
+        employee.getPostComments(item['id'])
+        .then(result => {
+          result.forEach(fetchComments);
+        });
+        // console.log("cek");
+      });
+      cardDiv.innerHTML = `<div class="card-body"><h5 class="card-title">`+item['title']+`</h5><p class="card-text">`+item['body']+`</p></div>`;
+      document.getElementById("postGrid").appendChild(cardDiv);
+    }
+
+    function fetchComments(item) {
+      var para = document.createElement("div");
+        para.innerHTML = `<div class="card-body"><h5 class="card-title">`+item['name']+`</h5><h6 class="card-subtitle mb-2 text-muted">`+ item['email'] +`</h6><p class="card-text">`+item['body']+`</p></div>`;
+        document.getElementById("commentGrid").appendChild(para);
+    }
+    const employee = new Employee();
+    // employee.getUserPosts(1)
+    employee.getUsers()
+      .then(result => {
+        result.forEach(fetchUsers);
+      });
+
+      // employee.getUserPosts(1)
+      // .then(result => {
+      //   result.forEach(fetchPosts);
+        
+      //   // console.log(result)
+      // });
+
+      // employee.getPostComments(1)
+      // .then(result => {
+      //   result.forEach(fetchComments);
+        
+      //   // console.log(result)
+      // });
     
   }
 };
